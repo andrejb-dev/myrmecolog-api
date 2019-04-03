a8b09205aadc12a4d81cd953388409312920324a
Zavael <cccp14@atlas.sk>
2019-04-03 12:29:42 +0200

    adding db.js

---
+++ src/db/db.js
@@ -0,0 +1,32 @@
+const db = require('pg');
+const client = new db({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+})
+
+module.exports = {
+    colonies: {
+        all: function () {
+            client.connect();
+            client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+                if (err) throw err;
+                for (let row of res.rows) {
+                    console.log(JSON.stringify(row));
+                }
+                client.end();
+            });
+        }
+    },
+    users: {
+        all: function () {
+            client.connect();
+            client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+                if (err) throw err;
+                for (let row of res.rows) {
+                    console.log(JSON.stringify(row));
+                }
+                client.end();
+            });
+        }
+    }
+}
